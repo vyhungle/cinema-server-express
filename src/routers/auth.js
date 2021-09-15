7a7f836c16d689aa4dd7e05c6e4b3419bdc2a67a
leevyx <lnhv.26112000@gmail.com>
2021-09-15 19:43:15 +0700

    update auth

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -20,7 +20,7 @@ function generateToken(user) {
   );
 }
 
-// @route POST api/auth/user
+// @route GET api/auth/user
 // @desc get my user
 // @access Private
 
@@ -30,10 +30,21 @@ router.get("/me", verifyToken, async (req, res) => {
     return res.json({
       success: true,
       message: "Lấy dữ liệu thành công",
-      user,
+      user: {
+        ...user._doc,
+        password: undefined,
+        profile: {
+          ...user._doc.profile,
+          address: {
+            ...user._doc.profile.address,
+            lat: undefined,
+            lng: undefined,
+          },
+        },
+      },
     });
   } catch (error) {
-    return res.json({
+    return res.status(500).json({
       success: false,
       message: "Lấy dữ liệu thất bại",
       error,
@@ -54,62 +65,69 @@ router.post("/register", async (req, res) => {
     dateOfBirth,
     address,
   } = req.body;
+  try {
+    const checkEmail = await User.findOne({ email: email });
+    const checkPhone = await User.findOne({ phoneNumber: phoneNumber });
 
-  const checkEmail = await User.findOne({ email: email });
-  const checkPhone = await User.findOne({ phoneNumber: phoneNumber });
-
-  const isEmail = checkEmail ? true : false;
-  const isPhone = checkPhone ? true : false;
-  const { valid, errors } = ValidateRegisterInput(
-    isEmail,
-    isPhone,
-    phoneNumber,
-    email,
-    password,
-    confirmPassword,
-    fullName,
-    address,
-    dateOfBirth
-  );
-  if (!valid) {
-    res.json({
-      success: false,
-      message: "Đăng ký tài khoản",
-      errors: errors,
-    });
-  } else {
-    const geo = await getGeoLocation(
-      `${address.street}, ${address.ward}, ${address.district}, ${address.city}`
-    );
-    const { lat, lng } = geo.data.results[0].geometry;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = new User({
-      email,
-      password: hashedPassword,
+    const isEmail = checkEmail ? true : false;
+    const isPhone = checkPhone ? true : false;
+    const { valid, errors } = ValidateRegisterInput(
+      isEmail,
+      isPhone,
       phoneNumber,
-      profile: {
-        fullName,
-        dateOfBirth,
-        address: { ...address, lat, lng },
-      },
-      createdAt: new Date().toISOString(),
-    });
-    const value = await newUser.save();
-    const token = generateToken(value);
-    res.json({
-      success: true,
-      message: "Đăng ký tài khoản",
-      values: {
-        token: token,
-        user: {
-          ...newUser._doc,
-          password: undefined,
-          profile: {
-            ...newUser._doc.profile,
-            address,
+      email,
+      password,
+      confirmPassword,
+      fullName,
+      address,
+      dateOfBirth
+    );
+    if (!valid) {
+      res.status(400).json({
+        success: false,
+        message: "Đăng ký tài khoản",
+        errors: errors,
+      });
+    } else {
+      const geo = await getGeoLocation(
+        `${address.street}, ${address.ward}, ${address.district}, ${address.city}`
+      );
+      const { lat, lng } = geo.data.results[0].geometry;
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        password: hashedPassword,
+        phoneNumber,
+        profile: {
+          fullName,
+          dateOfBirth,
+          address: { ...address, lat, lng },
+        },
+        createdAt: new Date().toISOString(),
+      });
+      const value = await newUser.save();
+      const token = generateToken(value);
+      res.json({
+        success: true,
+        message: "Đăng ký tài khoản",
+        values: {
+          token: token,
+          user: {
+            ...newUser._doc,
+            password: undefined,
+            profile: {
+              ...newUser._doc.profile,
+              address,
+            },
           },
         },
-      },
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Lỗi server!",
+      errors: error.message,
     });
   }
 });
@@ -119,53 +137,61 @@ router.post("/register", async (req, res) => {
 // @access Public
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const checkEmail = await User.findOne({ email: username });
-  const checkPhone = await User.findOne({ phoneNumber: username });
-  const { valid, errors } = ValidateLoginInput(username, password);
-  if (!checkEmail && !checkPhone) {
-    if (username !== "") {
-      errors.username = "Thông tin tài khoản không tồn tại";
-    }
-    res.json({
-      success: false,
-      message: "Đăng nhập",
-      errors: errors,
-    });
-  } else {
-    const user = checkEmail ? checkEmail : checkPhone;
-    delete user.password;
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      if (password !== "") {
-        errors.password = "Thông tin mật khẩu chưa chính xác";
+  try {
+    const checkEmail = await User.findOne({ email: username });
+    const checkPhone = await User.findOne({ phoneNumber: username });
+    const { valid, errors } = ValidateLoginInput(username, password);
+    if (!checkEmail && !checkPhone) {
+      if (username !== "") {
+        errors.username = "Thông tin tài khoản không tồn tại";
       }
-      res.json({
+      res.status(400).json({
         success: false,
         message: "Đăng nhập",
         errors: errors,
       });
     } else {
-      const token = generateToken(user);
-      res.json({
-        success: true,
-        message: "Đăng nhập",
-        values: {
-          token: token,
-          user: {
-            ...user._doc,
-            password: undefined,
-            profile: {
-              ...user._doc.profile,
-              address: {
-                ...user._doc.profile.address,
-                lat: undefined,
-                lng: undefined,
+      const user = checkEmail ? checkEmail : checkPhone;
+      delete user.password;
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        if (password !== "") {
+          errors.password = "Thông tin mật khẩu chưa chính xác";
+        }
+        res.status(400).json({
+          success: false,
+          message: "Đăng nhập",
+          errors: errors,
+        });
+      } else {
+        const token = generateToken(user);
+        res.json({
+          success: true,
+          message: "Đăng nhập",
+          values: {
+            token: token,
+            user: {
+              ...user._doc,
+              password: undefined,
+              profile: {
+                ...user._doc.profile,
+                address: {
+                  ...user._doc.profile.address,
+                  lat: undefined,
+                  lng: undefined,
+                },
               },
             },
           },
-        },
-      });
+        });
+      }
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Lỗi server!",
+      errors: error.message,
+    });
   }
 });
 
