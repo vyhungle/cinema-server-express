a29d09e0c0862afeddb1676319a66ba7d7127e01
leevyx <lnhv.26112000@gmail.com>
2021-09-26 20:31:38 +0700

    update model screen

--- src/models/Screen.js
+++ src/models/Screen.js
@@ -2,5 +2,7 @@ import { Schema, model } from "mongoose";
 
 const screenSchema = new Schema({
   name: String,
+  weekdayPrice: Number,
+  weekendPrice: Number,
 });
 module.exports = model("screens", screenSchema);
