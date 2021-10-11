061af0cd50b1a092c13699e0bad8c796fde8db1e
leevyx <lnhv.26112000@gmail.com>
2021-10-11 22:10:24 +0700

    update showtime

--- src/routers/showTime.js
+++ src/routers/showTime.js
@@ -3,32 +3,30 @@ const router = express.Router();
 import moment from "moment";
 
 import ShowTime from "../models/ShowTime"; //fix log
-import request from "supertest";
-import { addTicker } from "../api/serverAPI";
 import ShowTimeDetail from "../models/ShowTimeDetail";
 import { ValidateShowTime } from "../utils/validators";
 
 router.post("/add", async (req, res) => {
-  const { dateStart, dateEnd, screenDetailId, cinemaId, body } = req.body;
+  const { dateStart, dateEnd, screenDetailId, cinemaId, showTimes } = req.body;
   try {
     const { errors, valid } = ValidateShowTime(
       dateStart,
       dateEnd,
       screenDetailId,
       cinemaId,
-      body
+      showTimes
     );
     if (valid) {
-      const showTime = await ShowTime({
+      const newShowTime = await ShowTime({
         screenDetail: screenDetailId,
         cinema: cinemaId,
         status: true,
       });
-      await showTime.save();
+      await newShowTime.save();
 
       // add show time detail
 
-      body.map((item) => {
+      showTimes.map((item) => {
         item.times.map(async (time) => {
           const date_start = new Date(dateStart);
           const date_end =
@@ -43,13 +41,13 @@ router.post("/add", async (req, res) => {
               date: moment(date_start).format("L"),
             });
             if (!checkShowTimeDetail) {
-              const showTime = await ShowTimeDetail({
+              const showTimeDetail = await ShowTimeDetail({
                 date: moment(date_start).format("L"),
                 room: item.roomId,
-                showTime: item.showTimeId,
+                showTime: newShowTime._id,
                 timeSlot: time,
               });
-              await showTime.save();
+              await showTimeDetail.save();
             }
             date_start.setDate(date_start.getDate() + 1);
           } while (date_start <= date_end);
@@ -76,22 +74,37 @@ router.post("/add", async (req, res) => {
 });
 
 router.get("/all", async (req, res) => {
-  const showTimes = await ShowTime.find()
-    .populate("room")
-    .populate("timeSlot")
-    .populate({
-      path: "premiere",
-      populate: { path: "movie" },
-    })
-    .populate({
-      path: "premiere",
-      populate: { path: "screen" },
+  const { dateStart, dateEnd } = req.body;
+  try {
+    const date_start = new Date(dateStart);
+    const date_end =
+      dateEnd === "" || dateEnd === undefined || dateEnd === null
+        ? new Date(dateStart)
+        : new Date(dateEnd);
+    let showTimes = [];
+    do {
+      const showTimeList = await ShowTimeDetail.find({
+        date: moment(date_start).format("L"),
+      })
+        .populate("room")
+        .populate("timeSlot")
+        .populate("showTime");
+      showTimes = showTimes.concat(showTimeList);
+      date_start.setDate(date_start.getDate() + 1);
+    } while (date_start <= date_end);
+
+    return res.json({
+      success: true,
+      message: "Lấy danh sách lịch chiếu thành công",
+      showTimes,
     });
-  return res.json({
-    success: true,
-    message: "Lấy danh sách lịch chiếu thành công",
-    showTimes,
-  });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Lỗi 400!",
+      errors: error.message,
+    });
+  }
 });
 
 module.exports = router;
