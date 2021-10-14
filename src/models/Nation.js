f8c80779530a745e07e57402db84688b80e16294
Hùng Vỹ <lnhv.26112000@gmail.com>
2021-10-14 19:15:45 +0700

    cleam app

--- src/models/Nation.js
+++
@@ -1,6 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const nationSchema = new Schema({
-  name: String,
-});
-module.exports = model("nations", nationSchema);
--- src/routers/timeSlot.js
+++ src/routers/timeSlot.js
@@ -1,9 +1,6 @@
 import express from "express";
 const router = express.Router();
 import TimeSlot from "../models/TimeSlot";
-import Room from "../models/Room";
-import ShowTime from "../models/ShowTime";
-import RoomDetail from "../models/RoomDetail";
 
 router.post("/add", async (req, res) => {
   const { time } = req.body;
@@ -47,47 +44,5 @@ router.get("/all", async (req, res) => {
   }
 });
 
-router.get("/list-time", async (req, res) => {
-  const { dateStart, dateEnd, roomId } = req.body;
-  try {
-    // const start = new Date(dateStart);
-    // const end = new Date(dateEnd);
-
-    const showTimes = await ShowTime.find({ room: roomId, date: dateStart });
-    const roomDetail = await RoomDetail.find({ room: roomId }).populate(
-      "timeSlot"
-    );
-
-    let timeSlots = [];
-    if (showTimes) {
-      showTimes.map((item) => {
-        timeSlots.push(item.timeSlot);
-      });
-    }
-
-    if (timeSlots.length > 0) {
-      roomDetail.map((room, index) => {
-        timeSlots.map((time) => {
-          if (room.timeSlot._id.toString() === time.toString()) {
-            roomDetail.splice(index, 1);
-          }
-        });
-      });
-    }
-
-    let newTimeSlots = [];
-    roomDetail.map((item) => {
-      newTimeSlots.push(item.timeSlot);
-    });
-
-    return res.json({
-      success: true,
-      message: "lấy danh sách khung giờ thành công",
-      values: {
-        timeSlots: newTimeSlots.sort(),
-      },
-    });
-  } catch (error) {}
-});
 
 module.exports = router;
