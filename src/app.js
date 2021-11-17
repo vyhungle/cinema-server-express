6040d9b819aafddcfcc2f2b4c5d268454728b32d
Hùng Vỹ <lnhv.26112000@gmail.com>
2021-11-17 23:12:59 +0700

    a

--- src/app.js
+++ src/app.js
@@ -39,6 +39,11 @@ app.use(
     credentials: true,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set("json spaces", 2);
 
 //Router
