b607595ec1fe4be56ac6ee91cee6a73d0ddbb9f8
Tuyen <9a5.tuyen@gmail.com>
2021-11-20 12:41:19 +0700

    fix

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -286,7 +286,7 @@ export const ValidateMovie = (
     errors.dateStart = "Vui lòng chọn ngày bắt đầu.";
   } else if (!moment(dateStart).format("MM/dd/YYYY")) {
     errors.dateStart = "Ngày bắt đầu phải đúng format MM/dd/YYYY";
-  } else if (_dateStart < Date.now()) {
+  } else if (_dateStart <= Date.now()) {
     errors.dateStart =
       "Vui lòng chọn ngày bắt đầu lớn hơn hoặc bằng ngày hiện tại.";
   }
@@ -295,8 +295,6 @@ export const ValidateMovie = (
     errors.dateEnd = "Vui lòng chọn ngày kết thúc.";
   } else if (!moment(dateEnd).format("MM/dd/YYYY")) {
     errors.dateEnd = "Ngày kết thúc phải đúng format MM/dd/YYYY";
-  } else if (_dateStart <= Date.now()) {
-    errors.dateEnd = "Vui lòng chọn ngày kết thúc lớn hơn ngày hiện tại.";
   } else if (
     (_dateStart > _dateEnd && dateEnd.trim() !== "") ||
     _dateStart === _dateEnd
