bb2338a47d614ccde28ea52719095d8f1817ee3a
Hùng Vỹ <lnhv.26112000@gmail.com>
2021-12-02 23:05:02 +0700

    up code mua vé nhan theo ngày

--- src/routers/quick.js
+++ src/routers/quick.js
@@ -101,4 +101,102 @@ router.get("/get-show-time-detail-by-cinema-movie-date", async (req, res) => {
   }
 });
 
+router.get("/get-cinema-by-date", async (req, res) => {
+  try {
+    const { date } = req.query;
+    const showTimeDetails = await ShowTimeDetail.find({ date }).populate({
+      path: "showTime",
+      populate: "cinema",
+    });
+    const cinemas = [];
+    showTimeDetails.forEach((item) => {
+      const some = cinemas.some((x) => x._id === item.showTime.cinema._id);
+      if (!some) {
+        cinemas.push(item.showTime.cinema);
+      }
+    });
+    return res.json({
+      success: true,
+      message: "lấy danh sách rạp thành công",
+      values: {
+        cinemas,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: errorCatch,
+      errors: error.message,
+    });
+  }
+});
+
+router.get("/get-movie-by-cinema-date", async (req, res) => {
+  try {
+    const { cinemaId, date } = req.query;
+    const showTimeDetails = await ShowTimeDetail.find({ date }).populate({
+      path: "showTime",
+      populate: ["cinema", "movie"],
+    });
+    const movies = [];
+    showTimeDetails.forEach((item) => {
+      const some = movies.some((x) => x._id === item.showTime.movie._id);
+      if (!some) {
+        if (item.showTime.cinema._id == cinemaId) {
+          movies.push(item.showTime.movie);
+        }
+      }
+    });
+
+    return res.json({
+      success: true,
+      message: "Lấy danh sách phim thành công",
+      values: {
+        movies,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: errorCatch,
+      errors: error.message,
+    });
+  }
+});
+
+router.get("/get-movie-by-cinema", async (req, res) => {
+  try {
+    const { cinemaId } = req.query;
+    const now = new Date();
+    const date = `${now.getDay()}/${now.getMonth() + 1}/${now.getFullYear()}`;
+    const showTimeDetails = await ShowTimeDetail.find({ date }).populate({
+      path: "showTime",
+      populate: ["cinema", "movie"],
+    });
+    const movies = [];
+    showTimeDetails.forEach((item) => {
+      const some = movies.some((x) => x._id === item.showTime.movie._id);
+      if (!some) {
+        if (item.showTime.cinema._id == cinemaId) {
+          movies.push(item.showTime.movie);
+        }
+      }
+    });
+
+    return res.json({
+      success: true,
+      message: "Lấy danh sách phim thành công",
+      values: {
+        movies,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: errorCatch,
+      errors: error.message,
+    });
+  }
+});
+
 module.exports = router;
